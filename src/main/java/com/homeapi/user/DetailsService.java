34e27d70ffd66f6cc9450ca9e10384c14b56e9d0
Livia Galeazzi <liviagal@gmail.com>
2017-11-24 20:54:59 +0100

    Add UserDetailsService

---
+++ src/main/java/com/homeapi/user/DetailsService.java
@@ -0,0 +1,30 @@
+package com.homeapi.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DetailsService implements UserDetailsService
+{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        User user = userRepository.findByName(username);
+        if (user == null)
+        {
+            throw new UsernameNotFoundException(username + " was not found");
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getName(),
+                user.getPassword(),
+                AuthorityUtils.createAuthorityList(user.getRoles())
+        );
+    }
+}
