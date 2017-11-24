91e3088591d7e638b4dc6c9ffd616ec39a86f9d6
Livia Galeazzi <liviagal@gmail.com>
2017-11-24 21:03:12 +0100

    Add WebSecurityConfig

---
+++ src/main/java/com/homeapi/core/WebSecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.homeapi.core;
+
+import com.homeapi.user.DetailsService;
+import com.homeapi.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
+{
+    @Autowired
+    private DetailsService detailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.userDetailsService(detailsService)
+                .passwordEncoder(User.PASSWORD_ENCODER);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+}
