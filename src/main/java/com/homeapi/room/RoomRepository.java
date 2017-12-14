41b2eebba7b60f96915195d9ac0635cc4589b10e
Livia Galeazzi <liviagal@gmail.com>
2017-12-14 20:17:02 +0100

    Add search to room repository

--- src/main/java/com/homeapi/room/RoomRepository.java
+++ src/main/java/com/homeapi/room/RoomRepository.java
@@ -1,8 +1,11 @@
 package com.homeapi.room;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface RoomRepository extends PagingAndSortingRepository<Room, Long>
@@ -11,5 +14,9 @@ public interface RoomRepository extends PagingAndSortingRepository<Room, Long>
     @PreAuthorize("hasRole('ROLE_ADMIN') or #room.hasAdministrator(authentication.name)")
     Room save(@Param("room") Room entity);
 
+    @RestResource(path = "name", rel = "name")
+    Page<Room> findByNameContainingIgnoreCase(@Param("name") String name, Pageable pageable);
 
+    @RestResource(path = "area", rel = "area")
+    Page<Room> findByAreaLessThan(@Param("area") int area, Pageable pageable);
 }
