2e08ba317984d66799d13d012c35c882c362f0dc
Livia Galeazzi <liviagal@gmail.com>
2017-12-14 20:05:09 +0100

    Add partial name search to device repository

--- src/main/java/com/homeapi/device/DeviceRepository.java
+++ src/main/java/com/homeapi/device/DeviceRepository.java
@@ -1,7 +1,13 @@
 package com.homeapi.device;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface DeviceRepository extends PagingAndSortingRepository<Device, Long>
 {
+    @RestResource(path = "name", rel = "name")
+    Page<Device> findByNameContainingIgnoreCase(@Param("name") String name, Pageable pageable);
 }
