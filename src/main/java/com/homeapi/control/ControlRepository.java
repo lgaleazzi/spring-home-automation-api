57c0ebdf13ecbcceda5a237b56add1548ce93d20
Livia Galeazzi <liviagal@gmail.com>
2018-01-30 14:23:15 +0100

    Add check admin for device and control

--- src/main/java/com/homeapi/control/ControlRepository.java
+++ src/main/java/com/homeapi/control/ControlRepository.java
@@ -2,7 +2,12 @@ package com.homeapi.control;
 
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface ControlRepository extends PagingAndSortingRepository<Control, Long>
 {
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or #control.device.room.hasAdministrator(authentication.name)")
+    Control save(@Param("control") Control entity);
 }
--- src/main/java/com/homeapi/device/DeviceRepository.java
+++ src/main/java/com/homeapi/device/DeviceRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface DeviceRepository extends PagingAndSortingRepository<Device, Long>
 {
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or #device.room.hasAdministrator(authentication.name)")
+    Device save(@Param("device") Device entity);
+
     @RestResource(path = "name", rel = "name")
     Page<Device> findByNameContainingIgnoreCase(@Param("name") String name, Pageable pageable);
 }
